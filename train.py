09b1704456952fb7e8aad0ab4512995a7647b509
SilverChu96691 <silverchu@ieiworld.com>
2026-03-19 09:46:59 +0800

    Fix: Reverted LR to 0.1 because 0.9 was unstable

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
-# Version 2: High Learning Rate
-learning_rate = 0.9  # I think this will be faster!
+# Version 1: Simple Linear Regression
+learning_rate = 0.1
 print(f"Training started with LR: {learning_rate}")
